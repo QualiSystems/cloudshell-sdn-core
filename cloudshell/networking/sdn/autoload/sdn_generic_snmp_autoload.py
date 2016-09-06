46dfa0b41bda6565cd459dab93c5a9fb5e0da17f
LuizaNacshon <luiza.n@quali.com>
2016-09-06 12:03:44 +0300

    finished development - bofore testing

--- cloudshell/networking/sdn/autoload/sdn_generic_snmp_autoload.py
+++ cloudshell/networking/sdn/autoload/sdn_generic_snmp_autoload.py
@@ -7,21 +7,16 @@ __status__ = "Development"
 
 import re
 import inject
+import random
 from cloudshell.shell.core.driver_context import AutoLoadDetails
 from cloudshell.networking.autoload.networking_autoload_resource_attributes import NetworkingStandardRootAttributes
 from cloudshell.networking.sdn.configuration.cloudshell_controller_configuration import CONTROLLER_HANDLER
 from cloudshell.networking.sdn.configuration.cloudshell_controller_binding_keys import TOPOLOGY_HANDLER
 from cloudshell.shell.core.driver_context import AutoLoadAttribute
+from cloudshell.networking.autoload.networking_autoload_resource_structure import Port, Module
+
 class SDNGenericSNMPAutoload():
-
-    def __init__(self, controller_handler=None,network_handler = None, logger=None, vendor='ODL-Hellium'):
-        """Basic init with  handler and logger
-
-        :param snmp_handler:
-        :param logger:
-        :return:
-        """
-
+    def __init__(self, controller_handler=None,network_handler=None, logger=None, vendor='ODL-Hellium'):
 
         self._controller = controller_handler
         self._topology = network_handler
@@ -30,8 +25,10 @@ class SDNGenericSNMPAutoload():
         self.port_list = []
 
         self.relative_path = {}
+        self.resource_id = {}
 
-
+        self.switches_list = []
+        self.leaf_switches_list = []
         self.resources = list()
         self.attributes = list()
 
@@ -76,22 +73,19 @@ class SDNGenericSNMPAutoload():
         :return: AutoLoadDetails object
         """
 
-
-
-
         self.logger.info('*'*10)
-        self.logger.info('Starting huawei SNMP discovery process')
+        self.logger.info('Starting SDN SNMP Process')
         self.get_controller_properies()
 
 
-        self.get_switches_list()
-
+        self.get_leaf_switches_list()
+        self.get_switches_ports_dict()
+        self.build_relative_path()
         self.add_relative_paths()
-        self._get_chassis_attributes(self.chassis_list)
+
         self._get_ports_attributes()
-        self._get_module_attributes()
-        self._get_power_ports()
-        self._get_port_channels()
+        self._get_switches_attributes()
+
 
         result = AutoLoadDetails(resources=self.resources, attributes=self.attributes)
 
@@ -139,60 +133,87 @@ class SDNGenericSNMPAutoload():
         self.attributes.extend(root.get_autoload_resource_attributes())
         self.logger.info('Load controller Attributes completed.')
 
-    def get_switches_list(self):
+    def get_leaf_switches_list(self):
+        self.leaf_switches_list = self.topology.get_leaf_switches()
 
-    def get_edge_switches(self):pass
-    def get_edge_switches_ports(self):pass
+    def get_switches_ports_dict(self):
+        self.port_list = self.topology.get_switches_ports()
 
-    def _get_module_attributes(self):
-        """Set attributes for all discovered modules
 
-        :return:
-        """
+    def uniqueid(self):
+        seed = random.getrandbits(32)
+        while True:
+            yield seed
+            seed += 1
 
-        self.logger.info('Start loading Modules')
-        for module in self.module_list:
-            module_id = self.relative_path[module]
-            module_index = self._get_resource_id(module)
+    def build_relative_path(self):
+        unique_sequence = self.uniqueid()
+        for index, switch in enumerate(self.leaf_switches_list, start=1):
+            self.resource_id[switch] = int(next(unique_sequence))
+            self.relative_path[switch] = 0
+        for dedicated_switch in self.port_list:
+            for index, port in enumerate(self.port_list[dedicated_switch], start=1):
+                self.resource_id[port] = int(next(unique_sequence))
+                if self.resource_id.get(dedicated_switch) is not None:
+                    self.relative_path[port] = self.resource_id.get(dedicated_switch)
+
+    def add_relative_paths(self):
+
+        port_list = self.port_list
+        switches_list = self.leaf_switches_list
+
+        for switch in switches_list:
+            self.relative_path[switch] = self.relative_path[switch] + '/' + self.resource_id[switch]
+
+        for dedicated_switch in self.port_list:
+            for port in self.port_list[dedicated_switch]:
+                if (self.relative_path.get(port)):
+                    self.relative_path[port] = self.relative_path[port] + '/' + self.resource_id[port]
+
+    def _get_ports_attributes(self):
+
+        self.logger.info('Load Ports:')
+        for dedicated_switch in self.port_list:
+            for port in self.port_list[dedicated_switch]:
+                if "-" in port:
+
+                    interface_name = port.split("-")[1]
+                else:
+                    interface_name = port
+
+                if interface_name == '':
+                    continue
+
+                attribute_map = {'l2_protocol_type': '',
+                                 'mac': '',
+                                 'mtu': '',
+                                 'bandwidth': self.port_list[dedicated_switch][interface_name]['bandwidth'],
+                                 'description': '',
+                                 'adjacent': ''}
+                port_object = Port(name=interface_name, relative_path=self.relative_path[port], **attribute_map)
+                self._add_resource(port_object)
+                self.logger.info('Added ' + interface_name + ' Port')
+        self.logger.info('Load port completed.')
+
+    def _get_switches_attributes(self):
+
+        self.logger.info('Start loading Switches')
+        for switch in self.switches_list:
+            switch_id = self.relative_path[switch]
+            switch_index = self.resource_id[switch]
             module_details_map = {
-                'module_model': self.entity_table[module]['entPhysicalDescr'],
-                'version': self.snmp.get_property('ENTITY-MIB', 'entPhysicalSoftwareRev', module),
-                'serial_number': self.snmp.get_property('ENTITY-MIB', 'entPhysicalSerialNum', module)
+                'module_model': '',
+                'version': '',
+                'serial_number': ''
             }
 
-            if '/' in module_id and len(module_id.split('/')) < 3:
-                module_name = 'Module {0}'.format(module_index)
-                model = 'Generic Module'
-            else:
-                module_name = 'Sub Module {0}'.format(module_index)
-                model = 'Generic Sub Module'
-            module_object = Module(name=module_name, model=model, relative_path=module_id, **module_details_map)
-            self._add_resource(module_object)
 
-            self.logger.info('Module {} added'.format(self.entity_table[module]['entPhysicalDescr']))
-        self.logger.info('Load modules completed.')
+            switch_name = 'Sub Module {0}'.format(switch)
+            switch = 'SDN Switch'
+            switch_object = Module(name=switch_name, model=switch, relative_path=switch_id, **module_details_map)
+            self._add_resource(switch_object)
 
-    def _filter_lower_bay_containers(self):
-
-        upper_container = None
-        lower_container = None
-        containers = self.entity_mib_table.filter_by_column('Class', "container").sort_by_column('ParentRelPos').keys()
-        for container in containers:
-            vendor_type = self.snmp.get_property('ENTITY-MIB', 'entPhysicalVendorType', container)
-            if 'uppermodulebay' in vendor_type.lower():
-                upper_container = container
-            if 'lowermodulebay' in vendor_type.lower():
-                lower_container = container
-        if lower_container and upper_container:
-            child_upper_items_len = len(self.entity_mib_table.filter_by_column('ContainedIn', str(upper_container)
-                                                                           ).sort_by_column('ParentRelPos').keys())
-            child_lower_items = self.entity_mib_table.filter_by_column('ContainedIn', str(lower_container)
-                                                                   ).sort_by_column('ParentRelPos').keys()
-            for child in child_lower_items:
-                self.entity_mib_table[child]['entPhysicalContainedIn'] = upper_container
-                self.entity_mib_table[child]['entPhysicalParentRelPos'] = str(child_upper_items_len + int(
-                    self.entity_mib_table[child]['entPhysicalParentRelPos']))
-
+        self.logger.info('Load switches completed.')
 
 
     def _add_resource(self, resource):
@@ -203,61 +224,3 @@ class SDNGenericSNMPAutoload():
 
         self.resources.append(resource.get_autoload_resource_details())
         self.attributes.extend(resource.get_autoload_resource_attributes())
-
-
-
-
-
-    def _filter_entity_table(self, raw_entity_table):
-        """Filters out all elements if their parents, doesn't exist, or listed in self.exclusion_list
-
-        :param raw_entity_table: entity table with unfiltered elements
-        """
-
-        elements = raw_entity_table.filter_by_column('ContainedIn').sort_by_column('ParentRelPos').keys()
-        for element in reversed(elements):
-            parent_id = int(self.entity_mib_table[element]['entPhysicalContainedIn'])
-
-            if parent_id not in raw_entity_table or parent_id in self.exclusion_list:
-                self.exclusion_list.append(element)
-
-
-    def _get_device_details(self):
-        """Get root element attributes
-
-        """
-
-        self.logger.info('Start loading Switch Attributes')
-        result = {'system_name': self.sys_name,
-                  'vendor': self.vendor,
-                  'model': self._get_device_model(),
-                  'location': self.sys_location,
-                  'contact': self.sys_contact,
-                  'version': ''}
-        software_dscription = self.sys_descr
-        print software_dscription
-        match_version = re.search('Version\s+(?P<software_version>\S+)\S*\s+',
-                                  software_dscription)
-        if match_version:
-            result['version'] = match_version.groupdict()['software_version'].replace(',', '')
-
-        root = NetworkingStandardRootAttributes(**result)
-        self.attributes.extend(root.get_autoload_resource_attributes())
-        self.logger.info('Finished Loading Switch Attributes')
-
-
-
-    def _get_device_model(self):
-        """Get device model form snmp SNMPv2 mib
-
-        :return: device model
-        :rtype: str
-        """
-
-        result = ''
-        match_name = re.search(r'::(?P<model>\S+$)', self.snmp_object_id)
-        if match_name:
-            result = match_name.groupdict()['model'].capitalize()
-        return result
-
-
