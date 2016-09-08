0ea4407e4870c4506fd3a74d01aa4fc64843d86e
LuizaNacshon <luiza.n@quali.com>
2016-09-08 19:32:34 +0300

    add push function for static flows

--- cloudshell/networking/sdn/controller/controller_connection_handler.py
+++ cloudshell/networking/sdn/controller/controller_connection_handler.py
@@ -36,7 +36,6 @@ class SDNController(object):
         self.auth = HTTPBasicAuth(self.attributes['username'],self.attributes['password'])
 
     def get_query(self, northbound_api_component, query):
-
         data = dict()
         self._base_url = self.attributes['utl_prefix'] + self.attributes['ip'] + ':' + \
                          self.attributes['port'] + self.attributes['path']
@@ -54,4 +53,19 @@ class SDNController(object):
 
         return data
 
+
+    def post_static_flows(self,switch_id,flow_name,flow_data):
+        self._base_url = self.attributes['utl_prefix'] + self.attributes['ip'] + ':' + \
+                         self.attributes['port'] + self.attributes['path']
+        self.url = self._base_url + '/flowprogrammer/default/node/OF/' + switch_id + '/staticFlow/' + flow_name
+        response = requests.post(url=self.url, auth=self.auth,data=json.dumps(flow_data))
+
+        if response.status_code == 200:
+            data = response.json()
+            print data
+
+        else:
+            raise Exception('controller connection handler', 'query response is empty')
+
+        return data
 
